3d56eb24af88cf5bf45f1deb4b9b2030a0b6c6bb
lathindu <lathindu2000@gmail.com>
2023-01-12 22:37:14 +0530

    Learning Forward Declaration

--- Source/ToonTanks/BasePawn.h
+++ Source/ToonTanks/BasePawn.h
@@ -15,6 +15,7 @@ public:
 	// Sets default values for this pawn's properties
 	ABasePawn();
 
+
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
@@ -25,5 +26,13 @@ public:
 
 	// Called to bind functionality to input
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+
+
+
+private:
+	UPROPERTY()
+	class UCapsuleComponent* CapsuleComp;
+
 
 };
